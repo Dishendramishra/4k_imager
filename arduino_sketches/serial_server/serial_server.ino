f8d80d837a4fe4f3093941c4236da28bc3bb2451
unknown <dishendramishra@gmail.com>
2021-10-05 15:28:14 +0530

    integrated stepper2

--- arduino_sketches/serial_server/serial_server.ino
+++ arduino_sketches/serial_server/serial_server.ino
@@ -1,4 +1,5 @@
 #include <AccelStepper.h>
+#include <Servo.h>
 
 #define mot1_dirPin 2
 #define mot1_stepPin 3
@@ -7,27 +8,45 @@
 #define mot2_stepPin 5
 
 #define motorInterfaceType 1
+#define servo_pin 9
+
+Servo servo;   // nd_filter servo: motor ms90s
 
 AccelStepper stepper1 = AccelStepper(motorInterfaceType, mot1_stepPin, mot1_dirPin);
 AccelStepper stepper2 = AccelStepper(motorInterfaceType, mot2_stepPin, mot2_dirPin);
 
 bool flag = true;
 
-void go_home() {
-  while (analogRead(A0) < 1000) {
+void home_stepper1() {
+  while (analogRead(A1) < 1000) {
     stepper1.move(50);
     stepper1.runToPosition();
-    stepper1.setCurrentPosition(0);
-    Serial.println(analogRead(A0));
+//    Serial.println(analogRead(A1));
   }
+  stepper1.setCurrentPosition(0);
+}
+
+void home_stepper2() {
+  while (analogRead(A2) < 1000) {
+    stepper2.move(50);
+    stepper2.runToPosition();
+//    Serial.println(analogRead(A2));
+  }
+  stepper2.setCurrentPosition(0);
 }
 
 void setup(void) {
   Serial.begin(115200);
   while (!Serial);
 
+  servo.attach(servo_pin);
+  servo.write(0);
+  
   stepper1.setAcceleration(6400);
   stepper1.setMaxSpeed(6400);
+
+  stepper2.setAcceleration(6400);
+  stepper2.setMaxSpeed(6400);
 }
 
 void loop() { 
@@ -42,10 +61,15 @@ void loop() {
     }
     else if (str.startsWith("ser")) {
       int angle = str.substring(3).toInt();
+      servo.write(angle);
       Serial.println("done");
     }
-    else if (str.startsWith("home")) {
-      go_home();
+    else if (str.startsWith("homestep1")) {
+      home_stepper1();
+      Serial.println("done");
+    }
+    else if (str.startsWith("homestep2")) {
+      home_stepper2();
       Serial.println("done");
     }
     else if (str.startsWith("maf1_")) {
